2913ece6ff980e70e2ac492adadfa4b41c30f3c5
laouini <oumaima.laouini@sevetys.fr>
2024-01-23 17:39:08 +0100

    feat: Contenuation of previous commit: Additional file"

--- frontend/src/components/Pokemon/PokemonListItem.tsx
+++
@@ -1,9 +0,0 @@
-import { Pokemon } from './Pokemon';
-
-interface PokemonListItemProps {
-    pokemon: Pokemon;
-}
-
-export const PokemonListItem: React.FC<PokemonListItemProps> = ({ pokemon }) => (
-    <Pokemon key={pokemon.id} name={pokemon.name} id={pokemon.id} height={pokemon.height} weight={pokemon.weight} />
-);
